a4bbedb3000303e5afeafd5c7a9dc547899d8baf
BaptisteD <baptiste.dechamp@outlook.fr>
2025-03-08 14:36:10 +0100

    No insights

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-    <SpeedInsights />
   </React.StrictMode>,
 )
